be25f5595010c1d9cff371de33ca1c1462b16506
Your Name <you@example.com>
2023-05-09 00:53:34 +0100

    fix: MaintenanceControllerMigration

--- database/migrations/2023_03_02_130430_create_maintenances_table.php
+++ database/migrations/2023_03_02_130430_create_maintenances_table.php
@@ -24,9 +24,9 @@ return new class extends Migration
             $table->unsignedInteger('stade_id');
             $table->foreign('stade_id','stade_id_fk_5')->references('id')->on('stades')->onDelete('cascade');
 
-            $table->date('date_debut')->format('Y-m-d')->nullable();
+            $table->date('date_debut')->format('Y-m-d');
             $table->time('heure_debut');
-            $table->date('date_fin')->format('Y-m-d')->nullable();
+            $table->date('date_fin')->format('Y-m-d');
             $table->time('heure_fin');
             $table->string('description')->nullable(); //description de la nature de la maintenance
             $table->string('etat');
