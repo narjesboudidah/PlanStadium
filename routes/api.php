04c8a193b5aecde02b885051ee99cd0066c9e5b6
Your Name <you@example.com>
2023-05-09 00:39:11 +0100

    fix: MaintenanceControllerRoute

--- routes/api.php
+++ routes/api.php
@@ -61,9 +61,9 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/maintenance/{id}', [MaintenancesController::class, 'show']);//->middleware('superadmin', 'admin_ste');
     Route::put('/maintenance/{id}', [MaintenancesController::class, 'update']);//->middleware('superadmin', 'admin_ste');
     Route::delete('/maintenance/{id}', [MaintenancesController::class, 'destroy']);//->middleware('superadmin', 'admin_ste');
-    
-    Route::get('/maintenance/refuser/{id}', [ReservationsController::class, 'annulerReservation']);
-    Route::get('/maintenance/accepter/{id}', [ReservationsController::class, 'confirmerReservation']);
+
+    Route::get('/maintenance/refuser/{id}', [MaintenancesController::class, 'annulerMaintenance']);
+    Route::get('/maintenance/accepter/{id}', [MaintenancesController::class, 'confirmerMaintenance']);
 
     //equipes
     Route::get('/equipes', [EquipesController::class, 'index']);//->middleware('superadmin', 'admin_equipe');
